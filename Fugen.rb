5b6a08977d69490bf3cd87f94e6a159aa42fcc6f
Almaz Ibragimov <almazrafi@gmail.com>
2019-12-01 17:47:10 +0300

    Fugen 1.0.0-alpha.6 (#4)

--- Fugen.rb
+++ Fugen.rb
@@ -1,11 +1,11 @@
 class Fugen < Formula
-  version '1.0.0-alpha.5'
+  version '1.0.0-alpha.6'
 
   desc 'A Swift command line tool to generate code from Figma files'
   homepage 'https://github.com/almazrafi/Fugen'
   head 'https://github.com/almazrafi/Fugen.git'
   url "https://github.com/almazrafi/Fugen/archive/#{version}.tar.gz"
-  sha256 'e6fbdac3cc39923e9e3a53a1ed0079493d86b71dc2faf153b68449c117cb9717'
+  sha256 '9173ee63f07edccc3f42acb41828321904bc08e2cc2f4dfe6cb8406a3ae31cfe'
 
   depends_on :xcode => ['10.2', :build]
 
